06b1d4a124fd0bd142749d466bf371fe668a31e7
Victor Westerlund <victor.vesterlund@gmail.com>
2022-11-26 21:18:09 +0100

    feat: add asset loader

--- src/request/Request.php
+++ src/request/Request.php
@@ -4,6 +4,7 @@
 
 	class Request {
 
+		private static $preg_asset = "/^\/assets\/*/";
 		private static $preg_worker = "/^\/worker\/*/";
 
 		public function __construct() {
@@ -16,6 +17,10 @@
 		// Request router
 		private function router() {
 			switch ($this->path) {
+				// Get static asset from user content
+				case (preg_match(Request::$preg_asset, $this->path) ? true : false):
+					return $this->asset();
+				
 				// Return script to be run in a JS Worker
 				case (preg_match(Request::$preg_worker, $this->path) ? true : false):
 					return $this->worker();
@@ -41,7 +46,7 @@
 			}
 
 			// Strip leading slash
-			if (strpos($this->path, "/") === 1) {
+			if (strpos($this->path, "/") === 0) {
 				$path = substr($this->path, 1);
 			}
 
@@ -57,8 +62,10 @@
 
 		// Return contents of a JS file to be run inside a JS Worker
 		private function worker(): never {
+			$path = $this->get_requested_path();
+
 			// Check if worker script exists
-			$file = Path::pragma("src/frontend/js{$this->path}");
+			$file = Path::pragma("src/frontend/js/${path}");
 			if (!is_file($file)) {
 				http_response_code(404);
 				die("Not a worker");
@@ -66,6 +73,29 @@
 
 			// Return worker script contents
 			header("Content-Type: text/javascript");
+			exit(file_get_contents($file));
+		}
+
+		// Return the contents of a static asset from the user content folder.
+		// This should ideally be handled upstream by the webserver and is usually
+		// faster than going through PHP, and of course this code. But here we go.
+		private function asset(): never {
+			$file = Path::root($this->get_requested_path());
+
+			// Get MIME-type for file or default to plaintext
+			$type = mime_content_type($file);
+			if (!$type || $type === "application/x-empty") {
+				$type = "text/plain";
+			}
+
+			header("Content-Type: " . $type);
+
+			// File not found in user content /assets/<file>
+			if (!is_file($file)) {
+				http_response_code(404);
+				die("Asset not found");
+			}
+
 			exit(file_get_contents($file));
 		}
 	}
